bef720caf0c79f9c78f199bc7bc559cc02d91b7f
Aggelos Georgiadis <ag.gewr@gmail.com>
2025-11-12 13:20:39 +0200

    Initial Commit

---
+++ PropertyAdAppTests/PropertyAdAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  PropertyAdAppTests.swift
+//  PropertyAdAppTests
+//
+//  Created by Aggelos Georgiadis on 12/11/25.
+//
+
+import Testing
+
+struct PropertyAdAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
